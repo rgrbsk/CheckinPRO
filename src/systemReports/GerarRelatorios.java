26a5e99388b2a0f5360500915307bf0e67016ee4
Samii_Zanin <samiizanin@gmail.com>
2025-06-22 23:10:55 -0300

    Finalização - Último commit

--- src/systemReports/GerarRelatorios.java
+++ src/systemReports/GerarRelatorios.java
@@ -14,36 +14,23 @@ public class GerarRelatorios {
 
 	public static void gerarRecibo(int reservaId) {
 	    try {
-	        // Consulta SQL trazendo cliente, reserva e serviços
-	        String sql = "SELECT r.id AS reservaId, c.nome AS clienteNome, " +
-	                     "s.descricao AS servicoDescricao, s.valor AS servicoValor " +
-	                     "FROM reserva r " +
-	                     "JOIN cliente c ON r.id_cliente = c.id_cliente " +
-	                     "LEFT JOIN reserva_servico sr ON r.id = sr.reservas_id " +
-	                     "LEFT JOIN servicos_extra s ON sr.id_servico = s.id_servico " +
-	                     "WHERE r.id = ?";
-
-	        // Passando o ID da reserva como parâmetro
+	       
 	        List<Object> parametrosSQL = new ArrayList<>();
 	        parametrosSQL.add(reservaId);
 
-	        ResultSet rs = Conexao.executeQuery(sql, parametrosSQL);
-
+	       
 	        Map<String, Object> parametrosRelatorio = new HashMap<>();
 	        parametrosRelatorio.put("reservaId", reservaId);
 
-	       
 
-	        // Caminho do relatório compilado (.jasper)
 	        String caminhoRelatorio = "src/systemReports/ReciboPdf.jasper";
 	        JasperPrint jasperPrint = JasperFillManager.fillReport(caminhoRelatorio, parametrosRelatorio, Conexao.getConnection());
 
 	        // Exibir o relatório
 	        JasperViewer.viewReport(jasperPrint, false);
 
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	        System.out.println("Erro ao gerar recibo: " + e.getMessage());
-	    }
-	}
-}
+	    } catch (JRException e) {
+            e.printStackTrace();
+            System.err.println("Erro ao gerar o relatório: " + e.getMessage());
+        }
+	}}
